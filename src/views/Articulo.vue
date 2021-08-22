aa454923b523a94d84d4e4dfa5cf569b95d3a573
Jonathan Pumares <jepc1491@gmail.com>
2021-08-22 00:38:28 -0500

    Práctica consulta de posts del curso de VueJS utilizando Router.

---
+++ src/views/Articulo.vue
@@ -0,0 +1,40 @@
+<template>
+  <Titulo texto="Artículo con parámetros"/>
+  <h2>Parámetro: {{ $route.params.id }}</h2>
+  <h3>{{ post.title }}</h3>
+  <p>{{ post.id }} - {{ post.body }}</p>
+</template>
+
+<script>
+import Titulo from '../components/Titulo'
+
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      post: {}
+    }
+  },
+  methods: {
+    async getPost() {
+      try {
+        const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+        const item = await data.json()
+        console.log(item)
+        this.post = item
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created() {
+    this.getPost()
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,43 @@
+<template>
+  <Titulo texto="Título de mi blog"/>
+  <!-- <button @click="getPosts">Posts</button> -->
+  <div v-for="post in posts" :key="post.id">
+    <router-link :to="`/blog/${post.id}`">
+      {{ post.title }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Titulo from '../components/Titulo'
+
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    async getPosts() {
+      try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const array = await data.json()
+        console.log(array)
+        this.posts = array
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created() {
+    this.getPosts()
+  }
+}
+</script>
+
+<style>
+
+</style>
